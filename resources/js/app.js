890f445c040c673e0c1cfe0f82a6ac679ea5c6e9
Thibaud <thibaud29@protonmail.com>
2019-03-21 23:16:54 +0100

    add BootstrapVue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,13 @@
 require('./bootstrap');
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 window.Vue = require('vue');
-
+Vue.use(BootstrapVue)
 
 const files = require.context('./', true, /\.vue$/i);
 files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
-
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
 
 const app = new Vue({
     el: '#app'
