d5a53334aa058fd106f6e154e2cb1b09361c1b70
Thibaud <thibaud29@protonmail.com>
2019-04-06 13:48:57 +0200

    Add game show

---
+++ resources/js/components/admin/pages/games/AdminGameShow.vue
@@ -0,0 +1,229 @@
+<template>
+    <div>
+        <admin-content-header page_name="Game profile" ></admin-content-header>
+        <FlashMessage position="right bottom"></FlashMessage>
+
+        <section class="content">
+                <button @click="goToGamesList()" class="btn btn-primary">
+                    <i class="fas fa-arrow-left"></i>
+                    Return to game list
+                </button>
+            <div class="row">
+                <div class="col-md-3">
+                    <!-- Pseudo & Avatar -->
+                    <div class="box box-primary">
+                        <loader :color="'#337ab7'" v-show="loading"></loader>
+                        <div class="box-body box-profile" v-show="!loading">
+                            <img class="profile-user-img img-responsive img-circle" :src="game.image" alt="User profile picture">
+
+                            <h3 class="profile-username text-center"> {{ game.name }} </h3>
+                            <p class="text-muted text-center">{{ game.description }} </p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-9">
+                    <div class="nav-tabs-custom">
+                        <ul class="nav nav-tabs">
+                            <li class="active">
+                                <a href="#profile" data-toggle="tab">Profile</a>
+                            </li>
+                            <li v-show="!loading">
+                                <a href="#settings" data-toggle="tab">Settings</a>
+                            </li>
+                        </ul>
+                        <div class="tab-content">
+                            <div class="active tab-pane" id="profile">
+                                <h3> Game informations </h3>
+                                <loader :color="'#337ab7'" v-show="loading"></loader>
+
+                                <form class="form-horizontal" v-show="!loading">
+                                    <!-- Name -->
+                                    <div class="form-group">
+                                        <label for="name" class="col-sm-2 control-label">Name</label>
+
+                                        <div class="col-sm-10">
+                                            <input type="text" class="form-control" id="name" v-model="game.name" disabled>
+                                        </div>
+                                    </div>
+
+                                    <!-- Description -->
+                                    <div class="form-group">
+                                        <label for="description" class="col-sm-2 control-label">Description</label>
+
+                                        <div class="col-sm-10">
+                                            <textarea id="description" cols="30" rows="5" class="form-control" style="resize: none;" v-model="game.description" disabled></textarea>
+                                        </div>
+                                    </div>
+                                </form>
+
+                                <hr>
+                                <h3> Game players </h3>
+                                <loader :color="'#337ab7'" v-show="loading"></loader>
+
+                                <table class="table table-striped table-hover" v-show="!loading">
+                                    <thead>
+                                        <tr>
+                                            <th>Pseudo</th>
+                                            <th>Rank</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr v-for="(player, key) in gamePlayers" :key="key">
+                                            <td> {{ player.pseudo }} </td>
+                                            <td> {{ player.rank }} </td>
+                                            <td class="text-center">
+                                                <router-link :to="{ name: 'player.show', params: {id: player.id} }" class="btn btn-primary">
+                                                    <i class="fas fa-eye"></i>
+                                                </router-link>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+
+                            <div class="tab-pane" id="settings">
+                                <form @submit.prevent="updateGame()" class="form-horizontal">
+                                    <!-- Name -->
+                                    <div class="form-group">
+                                        <label for="name" class="col-sm-2 control-label">Name</label>
+
+                                        <div class="col-sm-10">
+                                            <input type="text" class="form-control" id="name" placeholder="Name" v-model="game.name" required>
+                                        </div>
+                                    </div>
+
+                                    <!-- Description -->
+                                    <div class="form-group">
+                                        <label for="description" class="col-sm-2 control-label">Description</label>
+
+                                        <div class="col-sm-10">
+                                            <textarea id="description" cols="30" rows="5" class="form-control" style="resize: none;" v-model="game.description" required></textarea>
+                                        </div>
+                                    </div>
+
+                                    <!-- Image -->
+                                    <div class="form-group">
+                                        <label for="image" class="col-sm-2 control-label">Image</label>
+
+                                        <div class="col-sm-10">
+                                            <input type="url" class="form-control" id="image" placeholder="https://..." v-model="game.image" required>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <div class="col-sm-offset-2 col-sm-10">
+                                            <button type="submit" class="btn btn-success">
+                                                <i class="fas fa-sync fa-spin" v-show="loading"></i>
+                                                <i class="fas fa-check" v-show="!loading"></i>
+                                                Update
+                                            </button>
+
+                                            <button type="button" class="btn btn-danger" @click="deleteGame(game.id)">
+                                                <i class="fas fa-trash-alt"></i>
+                                                Delete
+                                            </button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+import AdminContentHeader from './../../layouts/AdminContentHeader'
+import Loader from 'vue-spinner/src/ScaleLoader.vue'
+
+export default {
+    components: {
+        AdminContentHeader,
+        Loader
+    },
+
+    data() {
+        return {
+            loading: false,
+            game: {
+                id: this.$route.params.id,
+                name: "CS:GO",
+                description: "An amazing FPS shooter",
+                place: 5,
+                image: "http://i.imgur.com/VNk5wlL.png",
+            },
+            gameBeforeUpdate: {
+                id: this.$route.params.id,
+                name: "CS:GO",
+                description: "An amazing FPS shooter",
+                place: 5,
+                image: "http://i.imgur.com/VNk5wlL.png",
+            },
+            gamePlayers: [
+                {id: 1, pseudo: 'John Doe', email: 'john.doe@example.com', description: "I'm the best and i know it !", rank: "Eagle II"},
+                {id: 2, pseudo: 'John Doe', email: 'john.doe@example.com', description: "I'm the best and i know it !", rank: "Global Elite"},
+                {id: 6, pseudo: 'Gotaga'  , email: 'gotaga@example.com', description: "The french monster !", rank: "Sivler III"},
+                {id: 3, pseudo: 'John Doe', email: 'john.doe@example.com', description: "I'm the best and i know it !", rank: "Noob"},
+            ]
+        }
+    },
+
+    methods: {
+        // Go back to the previous page
+        goToGamesList() {
+            this.$router.push({name: 'games'});
+        },
+
+        // Update the game
+        updateGame() {
+            this.loading = true;
+            if (this.game.name != this.gameBeforeUpdate.name || 
+                this.game.description != this.gameBeforeUpdate.description ||
+                this.game.place != this.gameBeforeUpdate.place ||
+                this.game.image != this.gameBeforeUpdate.image) {
+
+                // Update player info before update
+                this.gameBeforeUpdate.name = this.game.name
+                this.gameBeforeUpdate.description = this.game.description
+                this.gameBeforeUpdate.place = this.game.place
+                this.gameBeforeUpdate.image = this.game.image
+                
+                this.flashMessage.success({
+                    title: "Game updated !",
+                    message: "The game has been successfully updated"
+                })
+            }
+            else {
+                this.flashMessage.error({
+                    title: "You didn't change any fields !",
+                    message: "You have to change a least one field to update the game"
+                })
+            }
+
+            this.loading = false;
+        },
+
+        // Delete the game
+        deleteGame(id) {
+            if (confirm("Are you sure you want to delete this team ? It's definitive")) {
+                this.flashMessage.success({
+                    title: "Game deleted !",
+                    message: "The game has been successfully deleted"
+                })
+                this.goToGamesList();
+            }
+
+            // this.flashMessage.error({
+            //     title: "Something went wrong",
+            //     message: "Please try again"
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
