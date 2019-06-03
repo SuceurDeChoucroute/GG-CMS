f7b9f1af34e975246e03f4fb938ca34c511e8646
Thibaud <thibaud29@protonmail.com>
2019-06-03 13:41:46 +0200

    add Shards css

--- resources/views/site.blade.php
+++ resources/views/site.blade.php
@@ -9,6 +9,8 @@
     <link rel="stylesheet" href="{{ asset('css/animation.css') }}">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
         integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href=" {{ asset('css/shards.min.css') }} ">
+    <link rel="stylesheet" href=" {{ asset('css/shards-extras.min.css') }} ">
     <title>Document</title>
 </head>
 <body>
@@ -28,5 +30,6 @@
 
 
     <script src=" {{ asset('js/app.js') }} "></script>
+    <script src=" {{ asset('js/shards.min.js') }} "></script>
 </body>
 </html>
