d284797cafef857b829a2f19d3b009f3ee77647f
Thibaud <thibaud29@protonmail.com>
2019-06-03 13:42:31 +0200

    Add rules page

---
+++ resources/js/components/site/pages/rules/SiteRules.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="row">
+        <div class="col-lg-8 mx-auto" v-html="rules">
+            <!-- {{ rules }} -->
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            loading: false,
+            rules: "",
+        }
+    },
+
+    methods: {
+        getRules() {
+            this.loading = true
+            axios.get('/api/rules')
+            .then(response => {
+                this.rules = response.data.content
+            })
+            .catch(() => {
+                alert('Something went wrong')
+            })
+        }
+    },
+
+    mounted() {
+        this.getRules()
+    }
+}
+</script>
+
+<style>
+
+</style>
--- resources/js/router/router_site.js
+++ resources/js/router/router_site.js
@@ -2,18 +2,27 @@ import Vue from 'vue';
 import Router from 'vue-router';
 Vue.use(Router);
 
-import Home from '../components/site/pages/Home.vue'
+// Home
+import SiteHome from '../components/site/pages/SiteHome.vue'
+
+// Auth
 import Login from '../components/site/pages/auth/Login.vue'
 import Register from '../components/site/pages/auth/Register.vue'
+
+// Rules
+import Rules from '../components/site/pages/rules/SiteRules.vue'
 
 
 const routes = [
     // Home
-    { name: "home", path: '/', component: Home },
+    { name: "home", path: '/', component: SiteHome },
 
     // Auth
     { name: "login", path: '/login', component: Login },
     { name: "register", path: '/register', component: Register },
+
+    // Rules
+    { name: "rules", path: '/rules', component: Rules },
 
 ];
 
