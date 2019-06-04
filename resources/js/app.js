319f520e459f7f9d73e5079f02bcbf9d59014bf1
Thibaud <thibaud29@protonmail.com>
2019-06-04 11:02:13 +0200

    add noty vuejs toast

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,10 +4,19 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import router from './router/router_site.js';
 import auth from './auth/auth.js';
+import VueNoty from 'vuejs-noty'
+import 'vuejs-noty/dist/vuejs-noty.css'
 
 window.Vue = require('vue');
 window.auth = auth;
 Vue.use(BootstrapVue)
+
+Vue.use(VueNoty, {
+    timeout: 3000,
+    theme: "bootstrap-v4",
+    progressBar: true,
+    layout: 'topRight'
+})
 
 const files = require.context('./', true, /\.vue$/i);
 files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
