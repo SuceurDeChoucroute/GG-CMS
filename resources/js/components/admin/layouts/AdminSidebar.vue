65e56a2cf2e64309ef8f37edbe2abd5f577088fc
Thibaud <thibaud29@protonmail.com>
2019-10-10 22:47:48 +0200

    fix show avatar in admin

--- resources/js/components/admin/layouts/AdminSidebar.vue
+++ resources/js/components/admin/layouts/AdminSidebar.vue
@@ -4,7 +4,7 @@
 
             <div class="user-panel">
                 <div class="pull-left image">
-                    <img src="https://gglan.fr/storage/avatars/pVZJ8PnP8ZkiapOtMiXtkzWrYVlxkGRuY1hQdgfQ.jpeg" class="img-circle" alt="User Image">
+                    <img :src="user.avatar" class="img-circle" alt="User Image">
                 </div>
                 <div class="pull-left info">
                     <p v-if="user"> {{ user.pseudo }} </p>
