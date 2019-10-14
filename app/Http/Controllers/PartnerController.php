140af3f72cc7a14ab5b01e92367989c16b7f697b
Thibaud <thibaud29@protonmail.com>
2019-10-14 21:50:40 +0200

    Partners Api

---
+++ app/Http/Controllers/PartnerController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Partner;
+use Illuminate\Http\Request;
+
+class PartnerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Partner::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $partner = Partner::create([
+            'name' => $request->name,
+            'site' => $request->site,
+            'logo' => $request->logo,
+        ]);
+
+        return $partner;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Partner  $partner
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Partner $partner)
+    {
+        return $partner;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Partner  $partner
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Partner $partner)
+    {
+        $partner->name = $request->name;
+        $partner->site = $request->site;
+        $partner->logo = $request->logo;
+        $partner->save();
+
+        return $partner;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Partner  $partner
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Partner $partner)
+    {
+        $partner->delete();
+
+        return response()->json([
+            'message' => 'Success',
+        ], 200);
+    }
+}
