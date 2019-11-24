4668cc45982f7ed44452b4fc96af53e71b2f6753
Thibaud <thibaud29@protonmail.com>
2019-11-24 22:54:30 +0100

    add Laravel Initializer

---
+++ app/Install.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use MadWeb\Initializer\Contracts\Runner;
+
+class Install
+{
+    public function production(Runner $run)
+    {
+        return $run
+            ->external('composer', 'install', '--no-dev')
+            ->artisan('key:generate', ['--force' => true])
+            ->artisan('migrate', ['--force' => true])
+            ->artisan('storage:link')
+            ->external('npm', 'install', '--production')
+            ->external('npm', 'run', 'production');
+    }
+
+    public function local(Runner $run)
+    {
+        return $run
+            ->external('composer', 'install')
+            ->artisan('key:generate')
+            ->artisan('migrate')
+            ->artisan('storage:link')
+            ->external('npm', 'install')
+            ->external('npm', 'run', 'development');
+    }
+}
