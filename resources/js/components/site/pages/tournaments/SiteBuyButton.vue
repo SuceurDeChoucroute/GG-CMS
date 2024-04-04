8244b0db356d78151c20efea703eee899368fef8
Thibaud P <25123473+ThobsChoucroute@users.noreply.github.com>
2024-04-04 17:19:20 +0200

    Revert "Revert "Update SiteBuyButton.vue""
    This reverts commit 4c86b0b82a573bfdca9ce3cef08f2a3d65c99e19.

--- resources/js/components/site/pages/tournaments/SiteBuyButton.vue
+++ resources/js/components/site/pages/tournaments/SiteBuyButton.vue
@@ -1,16 +1,21 @@
 <template>
-    <button class="btn btn-lg btn-success" title="Vous pouvez quand même payer votre entrée sur place avant la Lan" :disabled="alreadyPayed || loading" @click="payment()" v-if="tournament.stripe_key">
+    <!-- <button class="btn btn-lg btn-success" title="Vous pouvez quand même payer votre entrée sur place avant la Lan" :disabled="alreadyPayed || loading" @click="payment()" v-if="tournament.stripe_key">
         <i class="fas fa-plus" v-show="!loading"></i>
         <i class="fas fa-sync-alt fa-spin" v-show="loading"></i>
         Acheter ma place !
-    </button>
+    </button> -->
+
+    <a :href="tournament.stripe_key" target="_blank" class="btn btn-lg btn-success" title="Vous pouvez quand même payer votre entrée sur place avant la Lan" v-if="tournament.stripe_key">
+        <i class="fas fa-plus"></i>
+        Acheter ma place !
+    </a>
 </template>
 
 <script>
 export default {
     data() {
         return {
-            loading: false,
+            loading: false
         }
     },
 
@@ -21,40 +26,39 @@ export default {
     },
 
     methods: {
-        payment() {
-            if (!this.alreadyPayed) {
-                this.loading = true
+        // payment() {
+        //     if (!this.alreadyPayed) {
+        //         this.loading = true
 
-                let routeSuccess = process.env.MIX_APP_URL + "/api/payment/" + this.user.secret_key + "/" + this.tournament.id
-                let routeFail = process.env.MIX_APP_URL + "/api/payment/fail"
+        //         let routeSuccess = process.env.MIX_APP_URL + "/api/payment/" + this.user.secret_key + "/" + this.tournament.id
+        //         let routeFail = process.env.MIX_APP_URL + "/api/payment/fail"
                     
-                var stripe = Stripe(process.env.MIX_STRIPE_KEY, {
-                    betas: ['checkout_beta_3']
-                });
+        //         var stripe = Stripe(process.env.MIX_STRIPE_KEY, {
+        //             betas: ['checkout_beta_3']
+        //         });
     
-                // When the customer clicks on the button, redirect
-                // them to Checkout.
-                stripe.redirectToCheckout({
-                    items: [{ price: this.tournament.stripe_key, quantity: 1 }],
+        //         // When the customer clicks on the button, redirect
+        //         // them to Checkout.
+        //         stripe.redirectToCheckout({
+        //             items: [{ price: this.tournament.stripe_key, quantity: 1 }],
     
-                    // Note that it is not guaranteed your customers will be redirected to this
-                    // URL *100%* of the time, it's possible that they could e.g. close the
-                    // tab between form submission and the redirect.
-                    successUrl: routeSuccess,
-                    cancelUrl: routeFail,
-                })
-                .then(function (result) {
-                    this.loading = false
-                    if (result.error) {
-                        // If `redirectToCheckout` fails due to a browser or network
-                        // error, display the localized error message to your customer.
-                        var displayError = document.getElementById('error-message');
-                        displayError.textContent = result.error.message;
-                    }
-                });
-            }
-
-        },
+        //             // Note that it is not guaranteed your customers will be redirected to this
+        //             // URL *100%* of the time, it's possible that they could e.g. close the
+        //             // tab between form submission and the redirect.
+        //             successUrl: routeSuccess,
+        //             cancelUrl: routeFail,
+        //         })
+        //         .then(function (result) {
+        //             this.loading = false
+        //             if (result.error) {
+        //                 // If `redirectToCheckout` fails due to a browser or network
+        //                 // error, display the localized error message to your customer.
+        //                 var displayError = document.getElementById('error-message');
+        //                 displayError.textContent = result.error.message;
+        //             }
+        //         });
+        //     }
+        // },
     },
     
 }
